46163c6a56263e5aad3ba1857573a6992dda2348
dbuscombe-usgs <dbuscombe@usgs.gov>
2016-03-14 22:08:49 -0700

    Mon Mar 14 22:08:49 MST 2016

--- pysesa/__init__.py
+++ pysesa/__init__.py
@@ -64,7 +64,7 @@ For more information visit http://dbuscombe-usgs.github.io/pysesa/
     
 """
 
-__version__ = '0.0.21'
+__version__ = '0.0.22'
 
 #-----------------------------------------------------------------------------
 # Imports

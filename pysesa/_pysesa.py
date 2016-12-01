2b365544a1265356a9595caa2a72fdfda44c90a8
Daniel David Buscombe <ddb265@nau.froot.nau.edu>
2016-11-30 18:43:04 -0700

    filt 3

--- pysesa/_pysesa.py
+++ pysesa/_pysesa.py
@@ -430,7 +430,7 @@ def process(infile, out=1, detrend=4, proctype=1, mxpts=1024, res=0.05, nbin=20,
    del toproc_init
 
    ## number of points, undecimated
-   orig_pts = len(toproc_init)
+   orig_pts = len(toproc2)
 
    counter = 1
    for toproc in toproc2:
